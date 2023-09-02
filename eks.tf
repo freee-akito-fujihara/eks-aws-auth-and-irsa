e977cce1cf29723922d9b1b14d01531818829bb6
freee-akito-fujihara <akito-fujihara@c-fo.com>
2023-09-03 00:37:45 +0900

    irsaを作成

--- eks.tf
+++ eks.tf
@@ -71,3 +71,23 @@ provider "kubernetes" {
     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
+
+module "s3_irsa_access" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.30.0"
+
+  role_name = "s3-irsa-access"
+  allow_self_assume_role = false
+
+  oidc_providers = {
+    main = {
+      provider_arn = module.eks.oidc_provider_arn
+      namespace_service_accounts = [
+        "default:s3-irsa-access",
+      ]
+    }
+  }
+  role_policy_arns = {
+    s3 = aws_iam_policy.irsa-access.arn
+  }
+}
